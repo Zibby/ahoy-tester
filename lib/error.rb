ccb0c6b61effef3dc91a87e12c2dc616c40048c7
Matthew Mofrad <zibby@zibby.technology>
2019-07-25 14:42:37 +0100

    report error
    Signed-off-by: Matthew Mofrad <zibby@zibby.technology>

--- lib/error.rb
+++ lib/error.rb
@@ -1,8 +1,11 @@
-def error(e, message)
+def error(err, message)
   Slack.configure do |config|
     config.token = ENV['SLACK']
   end
   slack = Slack::Web::Client.new
-  puts message
-  slack.chat_postMessage(channel: ENV['CHANNEL'], text: message, as_user: true)
+  slack.chat_postMessage(
+    channel: ENV['CHANNEL'],
+    text: "#{message} because #{err}",
+    as_user: true
+  )
 end
