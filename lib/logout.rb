7315bbac401881ef487104512b95791ffa6bb47e
Matthew Mofrad <zibby@zibby.technology>
2019-07-25 14:00:24 +0100

    some rubocop stuff and typo fix
    Signed-off-by: Matthew Mofrad <zibby@zibby.technology>

--- lib/logout.rb
+++ lib/logout.rb
@@ -1,12 +1,12 @@
-require_relative "./error"
+require_relative './error'
 
+# Logout Check
 module Logout
   def logout
-    begin
-    profile =  @browser.find_element(css: '.btn-with-icon').click
+    @browser.find_element(css: '.btn-with-icon').click
     sleep 2
-    rescue StandardError => e
-      error(e, 'failed to logout/exit')
-    end
+
+  rescue StandardError => e
+    error(e, 'failed to logout/exit')
   end
 end
--- lib/org_check.rb
+++ lib/org_check.rb
@@ -1,18 +1,16 @@
-require_relative "./error"
+require_relative './error'
 
+# Org Check
 module OrgCheck
   def org_check
-    begin
-      @browser.navigate.to 'https://ahoy.alliantsdev.com'
-      @browser.find_element(css: '.tab-item').click
-      sleep 1
-      @browser.page_source.should include 'Organisations'
-      @browser.page_source.should include 'New Organisation'
-      @browser.page_source.should include 'Logo'
+    @browser.navigate.to 'https://ahoy.alliantsdev.com'
+    @browser.find_element(css: '.tab-item').click
+    sleep 1
+    @browser.page_source.should include 'Organisations'
+    @browser.page_source.should include 'New Organisation'
+    @browser.page_source.should include 'Logo'
 
-    rescue StandardError => e
-      error(e 'failed to load organisations'
-    end)
+  rescue StandardError => e
+    error(e, 'failed to load organisations')
   end
-  
 end
